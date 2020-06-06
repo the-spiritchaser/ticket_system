d9332626dc8454ce098f209b3088aeb019cd3544
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-06 21:20:17 +0900

    [add] new method to exec.rb

--- exec.rb
+++ exec.rb
@@ -2,3 +2,30 @@ require './customer'
 require './movie'
 require './ticket'
 require './ticket_vending_machine'
+
+customer = Customer.new("takashi", 5000)
+
+ticket_vending_machine = TicketVendingMachine.new
+
+titanic = Movie.new(
+                    "Titanic",
+                    2000,
+                    Date.new(2020, 2, 20),
+                    Date.new(2020, 4, 20)
+          )
+
+tom_and_jerry = Movie.new(
+                          "tom_and_jerry",
+                          1500,
+                          Date.new(2020, 3, 18),
+                          Date.new(2020, 5, 10)
+                )
+
+oceans_eleven = Movie.new(
+                          "Ocean's Eleven",
+                          1800,
+                          Date.new(2020, 4, 18),
+                          Date.new(2020, 6, 10)
+                )
+
+ticket_vending_machine.movies = [titanic, tom_and_jerry, oceans_eleven]
