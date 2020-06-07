04322759bc4d62a14392f073dbae848f010ac8c5
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-07 19:20:03 +0900

    [add] choose_movie

--- customer.rb
+++ customer.rb
@@ -1,5 +1,11 @@
 class Customer
+  attr_accessor :name, :money, :ticket
+
   def initialize(name, money, ticket=nil)
     @name, @money, @ticket = name, money, ticket
+  end
+
+  def choose_movie
+    gets.to_i
   end
 end
